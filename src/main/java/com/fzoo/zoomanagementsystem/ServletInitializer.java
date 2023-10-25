5af8400cb896d265e5b34a1ebd56ff27762889b7
Thanh Nguyen Hoang <nguyenhoangthanh30703@gmail.com>
2023-10-25 13:33:29 +0700

    test

---
+++ src/main/java/com/fzoo/zoomanagementsystem/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.fzoo.zoomanagementsystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.ResponseEntity;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZooManagementSystemApplication.class);
+	}
+
+}
---
+++ src/main/java/com/fzoo/zoomanagementsystem/model/Animal.java
@@ -0,0 +1,26 @@
+package com.fzoo.zoomanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Animal")
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private LocalDate dob;
+    private LocalDate dez;
+    private String sex;
+    private String specie;
+    private String status;
+    private int cageId;
+
+}
